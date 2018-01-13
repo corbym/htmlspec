f25e162b0fcdd5f8794fbb17817b70c3ebefcc37
Matt Corby <matthew.corby.eaglen@itv.com>
2018-01-13 19:53:17 +0000

    test panic, remove examples.

--- generator_test.go
+++ generator_test.go
@@ -3,40 +3,56 @@ package htmlspec_test
 import (
 	"github.com/corbym/gocrest/is"
 	. "github.com/corbym/gocrest/then"
-	"testing"
 	"github.com/corbym/gogiven/base"
 	"github.com/corbym/gogiven/generator"
 	"github.com/corbym/gogiven/testdata"
 	"github.com/corbym/htmlspec"
+	"testing"
 )
 
 var rawContent string
 var html string
+var gen = new(htmlspec.TestOutputGenerator)
 
-func TestGeneratorCreatesTestFirstTestHtml(testing *testing.T) {
+func TestTestOutputGenerator_Generate(testing *testing.T) {
 	fileIsConvertedToHtml()
 
 	AssertThat(testing, html, is.ValueContaining("<title>Generator Test</title>"))
 	AssertThat(testing, html, is.ValueContaining("<h1>Generator Test</h1>"))
+	AssertThat(testing, html, is.ValueContaining(`<pre class="highlight specification">`))
 	AssertThat(testing, html, is.ValueContaining("Given"))
 	AssertThat(testing, html, is.ValueContaining("When"))
 	AssertThat(testing, html, is.ValueContaining("Then"))
+	AssertThat(testing, html, is.ValueContaining("</pre>"))
+}
+
+func TestTestOutputGenerator_FileExtension(t *testing.T) {
+	AssertThat(t, gen.FileExtension(), is.EqualTo(".html"))
+}
+func TestTestOutputGenerator_Panics(t *testing.T) {
+	defer func() {
+		recovered := recover()
+		AssertThat(t, recovered, is.Not(is.Nil()))
+	}()
+	data.SomeMap = nil
+	fileIsConvertedToHtml()
+}
+
+var testingT = new(base.TestMetaData)
+var someMap = &base.SomeMap{"foo": base.NewSome(testingT,
+	"Generator Test",
+	base.NewTestMetaData("foo"),
+	"GivenWhenThen",
+	func(givens *testdata.InterestingGivens) {
+
+	}),
+}
+
+var data = &generator.PageData{
+	Title:   "Generator Test",
+	SomeMap: someMap,
 }
 
 func fileIsConvertedToHtml() {
-	testingT := new(base.TestMetaData)
-	someMap := &base.SomeMap{"foo": base.NewSome(testingT,
-		"Generator Test",
-		base.NewTestMetaData("foo"),
-		"Given\nWhen\nThen",
-		func(givens *testdata.InterestingGivens) {
-
-		}),
-	}
-	gen := new(htmlspec.TestOutputGenerator)
-	data := &generator.PageData{
-		Title:   "Generator Test",
-		SomeMap: someMap,
-	}
 	html = gen.Generate(data)
 }
--- htmlgenerator.go
+++ htmlgenerator.go
@@ -1,12 +1,16 @@
 package htmlspec
 
 import (
-	"path/filepath"
 	"bytes"
-	"os"
-	"html/template"
 	"github.com/corbym/gogiven/generator"
+	"html/template"
+	"os"
+	"path/filepath"
 )
+
+const goPathEnvKey = "GOPATH"
+const resourcesPath = "src/github.com/corbym/htmlspec/resources/"
+const baseTemplateName = "base"
 
 //TestOutputGenerator is an implmentation of the GoGivensOutputGenerator that generates an html file per
 // test.
@@ -15,7 +19,6 @@ type TestOutputGenerator struct {
 }
 
 // FileExtension for the output generated.
-
 func (generator *TestOutputGenerator) FileExtension() string {
 	return ".html"
 }
@@ -23,18 +26,18 @@ func (generator *TestOutputGenerator) FileExtension() string {
 // Generate generates the default output for a test. The return string contains the html
 // that goes into the output file generated in gogivens.GenerateTestOutput()
 func (generator *TestOutputGenerator) Generate(pageData *generator.PageData) string {
-	goPath := os.Getenv("GOPATH")
+	goPath := os.Getenv(goPathEnvKey)
 	tmpl := template.Must(template.ParseFiles(
-		filepath.Join(goPath, "src/github.com/corbym/htmlspec/resources/htmltemplate.gtl"),
-		filepath.Join(goPath, "src/github.com/corbym/htmlspec/resources/capturedio.gtl"),
-		filepath.Join(goPath, "src/github.com/corbym/htmlspec/resources/interestinggivens.gtl"),
-		filepath.Join(goPath, "src/github.com/corbym/htmlspec/resources/style.gtl"),
-		filepath.Join(goPath, "src/github.com/corbym/htmlspec/resources/test-body.gtl"),
-		filepath.Join(goPath, "src/github.com/corbym/htmlspec/resources/contents.gtl"),
-		filepath.Join(goPath, "src/github.com/corbym/htmlspec/resources/javascript.gtl"),
+		filepath.Join(goPath, resourcesPath+"htmltemplate.gtl"),
+		filepath.Join(goPath, resourcesPath+"capturedio.gtl"),
+		filepath.Join(goPath, resourcesPath+"interestinggivens.gtl"),
+		filepath.Join(goPath, resourcesPath+"style.gtl"),
+		filepath.Join(goPath, resourcesPath+"test-body.gtl"),
+		filepath.Join(goPath, resourcesPath+"contents.gtl"),
+		filepath.Join(goPath, resourcesPath+"javascript.gtl"),
 	))
 	var buffer = new(bytes.Buffer)
-	err := tmpl.ExecuteTemplate(buffer, "base", pageData)
+	err := tmpl.ExecuteTemplate(buffer, baseTemplateName, pageData)
 	if err != nil {
 		panic(err.Error())
 	}
